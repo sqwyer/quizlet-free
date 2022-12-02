144483c9c3704e65665cfc8e93e460b96994691b
Sawyer Bivens <570000826@nlrsd.org>
2022-12-02 14:24:43 -0600

    Start on 'games'

--- packages/web/public/src/study.tsx
+++ packages/web/public/src/study.tsx
@@ -14,6 +14,8 @@ type ApiResponse = {
     error?: string
 }
 
+type Game = "Learn" | "Test"
+
 function Err() {
     return (
         <div>
@@ -22,43 +24,110 @@ function Err() {
     )
 }
 
-function Study(studyset: StudySet, url: string) {
+function LearnGame() {
     return (
-        <div className="studyset">
-            <h1>{studyset.title}</h1>
-            <div className="row">
-                <p>{studyset.cards.length} cards</p>
-                <a href={url} target="_blank" className="link">Open in Quizlet</a>
-            </div>
-            <div className="row nav">
-                <div className="game">
-                    <span>Learn</span>
+        <div id="game-container"></div>
+    )
+}
+function TestGame() {
+    return (
+        <div id="game-container"></div>
+    )
+}
+
+function Game({game, studyset, url}: {game: Game, studyset: StudySet, url: string}) {
+    return (
+        <nav>
+            <div id="game-label" onclick={() => document.getElementById('game-dropdown').classList.toggle('shown')}>
+                <div className="label">
+                    <span>{game}</span>
+                    <span>
+                        <img src="public/chevron-down.png" />
+                    </span>
                 </div>
-                <div className="game">
-                    <span>Test</span>
+                <div className="dropdown" id="game-dropdown">
+                    <div>
+                        <div className="dropdown-section">
+                            <span>
+                                <img src="public/learn.png" />
+                                <p>Learn</p>
+                            </span>
+                            <span>
+                                <img src="public/test.png" />
+                                <p>Test</p>
+                            </span>
+                        </div>
+                        <div className="dropdown-section">
+                            <span>Home</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="right">
+                <button class="btn hollow" onclick={() => {populate(Study, {studyset, url})}}>
+                    &times;
+                </button>
+            </div>
+            {game == "Learn" ? LearnGame() : TestGame()}
+        </nav>
+    )
+}
+
+function Nav() {
+    return (
+        <nav>
+            <a id="title" href="/">
+                Quizlet Free
+            </a>
+            <div id="right">
+                <a href="https://github.com/sqwyer/quizlet-free" id="github-link" target="_blank">
+                    <img src="public/github.png" />
+                </a>
+            </div>
+        </nav>
+    )
+}
+
+function Study({studyset, url}: {studyset: StudySet, url: string}) {
+    return (
+        <div>
+            {Nav()}
+            <div className="studyset">
+                <h1>{studyset.title}</h1>
+                <div className="row">
+                    <p>{studyset.cards.length} cards</p>
+                    <a href={url} target="_blank" className="link">Open in Quizlet</a>
+                </div>
+                <div className="row nav">
+                    <div className="game" onclick={() => {populate(Game, {game: 'Learn', studyset, url})}}>
+                        <span>Learn</span>
+                    </div>
+                    <div className="game">
+                        <span>Test</span>
+                    </div>
                 </div>
             </div>
         </div>
     )
 }
 
-function populate(res: ApiResponse, url: string) {
-    const container = document.getElementById('content');
+function populate<T>(component: (props: T) => any, props?: T, parent?: string) {
+    const container = document.getElementById(parent ?? 'content');
     [...container.children].forEach(child => child.remove());
-
-    if(res.error) {
-        container.append(Err())
-    } else {
-        container.append(Study(res.data, url))
-    }
+    container.append(component(props))
 }
 
 (async function() {
     try {
         const url = new URLSearchParams(window.location.search).get("url");
-        const studyset: ApiResponse = await (await fetch(`/api/get_set?url=${url}`)).json();
-        populate(studyset, url);
+        const res: ApiResponse = await (await fetch(`/api/get_set?url=${url}`)).json();
+        if(res.error) populate(Err);
+        else populate(Study, {studyset: res.data, url});
     } catch(err) {
         console.error(err);
     }
 })();
+
+document.addEventListener('click', event => {
+    console.log(event.target);
+})
